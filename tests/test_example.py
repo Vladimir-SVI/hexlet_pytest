22c8607a4fa97463a9cc9182836e6e4b6450a8c6
SVI <svinovoch@yandex.ru>
2023-04-03 20:37:10 +0300

    add tests file

---
+++ tests/test_example.py
@@ -0,0 +1,8 @@
+from example import reverse
+#from hexlet_pytest.example import reverse
+
+def test_reverse():
+    assert reverse('Hexlet') == 'telxeH'
+
+def test_reverse_for_empty_string():
+    assert reverse('') == ''
